8ad94ddb9fd09ac1f20852bd402ad7d1fc9b1383
Trammell Hudson <hudson@trmm.net>
2022-02-15 18:04:57 +0000

    nic: polling timer works for receiving packets

--- nic.c
+++ nic.c
@@ -6,6 +6,7 @@
 #include <linux/kernel.h>
 #include <linux/netdevice.h>
 #include <linux/etherdevice.h>
+#include <linux/timer.h>
 #include "efiwrapper.h"
 #include "efinet.h"
 
@@ -13,8 +14,92 @@ typedef struct {
 	spinlock_t lock;
 	EFI_SIMPLE_NETWORK_PROTOCOL * uefi_nic;
 	int id;
+	int up;
+	struct net_device * dev;
 	struct net_device_stats stats;
+	struct sk_buff * rx_skb;
 } uefi_nic_t;
+
+#define MAX_NICS 16
+static struct timer_list uefi_rx_timer;
+static uefi_nic_t * uefi_nics[MAX_NICS];
+static int uefi_nic_count;
+
+static int uefi_net_rx(uefi_nic_t * nic)
+{
+	struct sk_buff * skb;
+	UINTN pkt_len = 1500;
+	unsigned long flags;
+	int status;
+
+	// if we are no longer up, stop scheduling
+	if (!nic->up)
+		return 0;
+
+	if (nic->rx_skb == NULL)
+	{
+		// replenish our rx packet and align it
+		nic->rx_skb = netdev_alloc_skb(nic->dev, pkt_len + 5);
+		skb_reserve(nic->rx_skb, NET_IP_ALIGN);
+	}
+
+	spin_lock_irqsave(&nic->lock, flags);
+	skb = nic->rx_skb;
+
+	status = nic->uefi_nic->Receive(
+		nic->uefi_nic,
+		NULL, // header size, no processing required
+		&pkt_len,
+		skb_tail_pointer(skb),
+		NULL, // src addr
+		NULL, // dst addr,
+		NULL // proto
+	);
+
+	// if we have successful in receiving the packet
+	// remove this skb from the rx pool
+	if (status == 0)
+		nic->rx_skb = NULL;
+
+	spin_unlock_irqrestore(&nic->lock, flags);
+
+	if (status == 6) // EFI_NOT_READY)
+	{
+		// no packet to receive
+	} else
+	if (status == 0)
+	{
+		// success! update the actual length received
+		// which has already been copied into the buffer
+		skb_put(skb, pkt_len);
+		skb->protocol = eth_type_trans(skb, nic->dev);
+		//printk("uefi%d: rx %lld bytes proto %d\n", nic->id, pkt_len, skb->protocol);
+		netif_rx(skb);
+	} else {
+		printk("uefi%d: error %d\n", nic->id, status);
+	}
+
+	return 1;
+}
+
+static void uefi_net_poll(struct timer_list * timer)
+{
+	uefi_memory_map_add();
+
+	// try to clear the queues on the NICs
+	for(int loops = 0 ; loops < 10 ; loops++)
+	{
+		int count = 0;
+		for(int i = 0 ; i < uefi_nic_count ; i++)
+			count += uefi_net_rx(uefi_nics[i]);
+		if (count == 0)
+			break;
+	}
+
+	// reschedule our selves to check again
+	mod_timer(timer, jiffies + msecs_to_jiffies(5));
+}
+
 
 static int uefi_net_open(struct net_device * dev)
 {
@@ -31,6 +116,8 @@ static int uefi_net_open(struct net_device * dev)
 	}
 
 	printk("uefi%d: started nic\n", nic->id);
+	nic->up = 1;
+
 	return 0;
 }
 
@@ -40,6 +127,8 @@ static int uefi_net_stop(struct net_device * dev)
 	int status;
 
 	uefi_memory_map_add();
+
+	nic->up = 0; // we'll stop scheduling timers
 
 	status = nic->uefi_nic->Stop(nic->uefi_nic);
 	if (status != 0)
@@ -144,8 +233,11 @@ static int uefi_nic_create(int id, EFI_SIMPLE_NETWORK_PROTOCOL * uefi_nic)
 	memset(nic, 0, sizeof(*nic));
 
 	spin_lock_init(&nic->lock);
+	nic->dev = dev;
 	nic->uefi_nic = uefi_nic;
 	nic->id = id;
+	nic->up = 0;
+	nic->rx_skb = NULL;
 
 	memcpy(dev->dev_addr, uefi_nic->Mode->CurrentAddress.Addr, ETH_ALEN);
 	dev->netdev_ops = &uefi_nic_ops;
@@ -163,6 +255,9 @@ static int uefi_nic_create(int id, EFI_SIMPLE_NETWORK_PROTOCOL * uefi_nic)
 		dev->dev_addr[4],
 		dev->dev_addr[5]
 	);
+
+	// store it in our array of active NIC
+	uefi_nics[uefi_nic_count++] = nic;
 
 	return 0;
 }
@@ -193,6 +288,9 @@ int uefi_nic_init(void)
 
 		uefi_nic_create(i, nic);
 	}
+
+	timer_setup(&uefi_rx_timer, uefi_net_poll, 0);
+	mod_timer(&uefi_rx_timer, jiffies + msecs_to_jiffies(10));
 
 	return 0;
 }
